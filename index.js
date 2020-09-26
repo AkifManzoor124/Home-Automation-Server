663c0ec3089858783dd0429bd92b33c8bebd3f9a
Akif Manzoor <akifmanzoor124@gmail.com>
2020-09-26 19:58:37 -0400

    created get and post requests for the toggle button for the frontend

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port = 3005
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(express.urlencoded());
+
+const deviceStatus = {
+  "BatmanLight": false
+}
+
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
+
+app.get('/api/devices', (request, response) => {
+  response.send(deviceStatus)
+})
+
+app.post('/api/devices', (request, response) => {
+  const status = request.body
+  deviceStatus["BatmanLight"] = request.body["BatmanLight"]
 })
 
 app.listen(port, () => {
